2225780a9442fc087156f67702a0119d292b1657
Von C <shervonc@gmail.com>
2019-07-29 21:56:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign ({}, object,{[key]: value})
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({},object)
+  delete newObj[key]
+  return newObj
+}
